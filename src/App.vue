aee02bddc2385fa97a1e81cfb2fa16ae4ef3c48f
Irena Koval <i.irenakoval@gmail.com>
2024-10-23 19:20:50 +0300

    feat: add Footer

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,12 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import Header from './layout/Header.vue'
+import Footer from './layout/Footer.vue'
 </script>
 
 <template>
   <Header />
 
   <RouterView />
+  <Footer />
 </template>
-
-<style scoped>
-
-</style>
---
+++ src/layout/Footer.vue
@@ -0,0 +1,22 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  data() {
+    year: new Date().getFullYear() as number
+  },
+})
+</script>
+
+<template>
+  <div class="flex py-8 items-center justify-center">
+    <p>
+      &copy; {{ year }}
+      <a
+        href="https://irena-koval-portfolio.netlify.app/"
+        alt="Irena Koval Portfolio"
+        class="text-blue-500"
+        >irenako</a
+      >. All rights reserved.
+    </p>
+  </div>
+</template>
